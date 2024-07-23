5ac18718a30aeb0fc0165f8cc36d41fe3b6664eb
gixi <gixipixe@gmail.com>
2024-07-22 21:06:35 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,6 @@ function App() {
     <main className="flex flex-col items-center justify-start h-full w-[100vw] sm:w-[550px] font-mono">
       <Toaster position="top-rigth" reverseOrder={false} />
       <Form />
-      <a
-        rel="noreferrer"
-        href="https://gixi.dev"
-        target="_blank"
-        className="text-slate-300 text-md sm:text-lg hover:text-white cursor-pointer w-full text-center pt-6"
-        title="gixi contacto"
-      >
-        Creado con ❤️ por <u>gixi</u>
-      </a>
     </main>
   );
 }
